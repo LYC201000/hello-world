db5efb20d2544cf4a60166f54c533f7da10ac616
Administrator <Administrator@hyup>
2018-05-01 12:24:07 +0900

    186p 4-15

---
+++ 뇌를 자극하는 자바/src/ConditionlOpExampel.java
@@ -0,0 +1,16 @@
+/**
+ * 2018. 5. 1. dev By lee.Y.C
+   
+   ConditionlOpExampel.java
+ */
+
+public class ConditionlOpExampel {
+
+	public static void main(String[] args) {
+
+		int a=20,b=30, max;
+		max	=	a	<	b	?	a:b;
+		System.out.println(max);
+	}
+
+}
