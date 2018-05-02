0dc5bba37373461792a9d1f0b732a49d4dc3f7fc
Administrator <Administrator@hyup>
2018-05-02 10:27:42 +0900

    111111

--- 뇌를 자극하는 자바/src/Circle.java
+++ 뇌를 자극하는 자바/src/Circle.java
@@ -9,13 +9,26 @@
  *
  */
 public class Circle {
-
-	/**
-	 * @param args
-	 */
+	public int radius;
+	public String name;
+	
+	public Circle() { // 매개변수가 없는 생성자
+		radius=1;
+		name= "";
+	}
+	//매개변수를 가진 생성자
+	public Circle(int r, String n) {
+		radius=r;
+		name= n;
+	}
+	public double getarea() { //메소드
+		return 3.14*radius*radius;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Circle pizza=new Circle(10,"w자바피자");//Circle 객체 생성, 반지름 10
+		double area=pizza.getarea();
+		System.out.println(pizza.name+"의먼적은"+area);
+		
 	}
 
 }
