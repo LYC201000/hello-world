0af7019c5cf87e38e6074b8644bdea95453242c8
Administrator <Administrator@hyup>
2018-05-10 12:48:04 +0900

    0510,  0509수정

--- 뇌를 자극하는 자바/src/JDBCExample1.java
+++ 뇌를 자극하는 자바/src/JDBCExample1.java
@@ -1,5 +1,6 @@
 
-import java.sql.*;// 패키지
+import java.sql.*;// 패키지   
+//db접속 
 
 class JDBCExample1 {
     public static void main(String args[]) {
---
+++ 뇌를 자극하는 자바/src/PrintfExamplel.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class PrintfExamplel {
+	public static void main(String args[]) {
+		System.out.printf("%+d%n",10);
+		System.out.printf("%+d%n",-10);
+		System.out.printf("가격:%,d원%n",10000000);
+		Calendar now = new GregorianCalendar();		
+	/*	System.out.printf(
+				"%1$tY/%1$tm/%1$td %1$tH:%1$tM:%1$TtS %n", now);
+		System.out.printf(
+				"%1$tY년/%1$tm월/%1$td일 %1$tp %1$tH시:%1$tM분:%1$tS초 %n",now);
+		System.out.printf(Locale.US,"%tc %n", now);*/
+		
+		System.out.printf(Locale.US,"%tc %n", now);
+		
+	}
+	
+}
